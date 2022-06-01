55751feaa04e1f92e3a5e77963e5ff2de13c1790
gingercook <vahitaras@gmail.com>
2022-06-01 19:16:28 -0400

    getStaticPaths added

---
+++ pages/[pid].js
@@ -0,0 +1,50 @@
+// fs is a nodejs module
+// path is a nodejs module
+// This module will not appear in Client Side App
+import path from 'path';
+import fs from 'fs/promises';
+
+const ProductDetailPage = props => {
+    const { loadedProduct } = props;
+
+    return (
+        <>
+            <h1>{loadedProduct.title}</h1>
+            <p>{loadedProduct.description}</p>
+        </>
+    )
+}
+
+// NextJS-Server-Side-Dynamic-Link-Parameter-Extraction
+// Server side dynamic link parameter extraction
+export const getStaticProps = async (context) => {
+    const { params } = context;
+
+    const productId = params.pid;
+
+    const filePath = path.join(process.cwd(), 'data/dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    const product = data.products.find(product => product.id === productId);
+
+    return {
+        props: {
+            loadedProduct: product
+        }
+    };
+};
+
+export const getStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { pid: 'p1' } },
+            { params: { pid: 'p2' } },
+            { params: { pid: 'p3' } }
+        ],
+
+        fallback: false
+    };
+};
+
+export default ProductDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 // fs is a nodejs module
+// path is a nodejs module
 // This module will not appear in Client Side App
 import path from 'path';
 import fs from 'fs/promises';
+
+import Link from 'next/link';
 
 const HomePage = (props) => {
   const { products } = props;
@@ -11,7 +14,9 @@ const HomePage = (props) => {
       {products.map(product => (
         <li
           key={product.id}>
+          <Link href={`/${product.id}`}>
             {product.title}
+          </Link>
         </li>
       ))}
     </ul>
@@ -21,9 +26,23 @@ const HomePage = (props) => {
 // NextJS-getStaticProps-Server-Side-Rendering
 // Code in getStaticProps is not shipped to the client
 export const getStaticProps = async () => {
-  const filePath = path.join(process.cwd(), 'data/dummy-backend.json' );
+  const filePath = path.join(process.cwd(), 'data/dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // redirect return object key for SSR
+  if (!data) {
+    return {
+      redirect: {
+        destiunation: '/no-data',
+      }
+    }
+  }
+
+  // notFound return object key for SSR
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -38,7 +57,12 @@ export const getStaticProps = async () => {
     // In development server, we will always see the latest data no matter if
     // this option is given. But in production, this section matters.
     // This is called incremental static regeneration.
-    revalidate: 2
+    revalidate: 2,
+
+    // notFound: true,
+    // redirect: {
+    //   destination: '/no-data'
+    // },
   };
 };
 
