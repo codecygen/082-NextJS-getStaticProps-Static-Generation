5778d3b09bee0cbc3fc4bc69f7acd4e73ffa1d26
gingercook <vahitaras@gmail.com>
2022-06-01 18:38:15 -0400

    ISR section added into SSR

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a simple Next.js app.
+
+When you run "npm run dev" server files are optimized and ".next" folder gets created.
+"npm start" starts the server with the files under ".next" folder. These files are for production.
+
+## Keywords:
+- NextJS-getStaticProps-Server-Side-Rendering
+- NextJS-ISR-Incremental-Static-Regeneration
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,45 @@
+// fs is a nodejs module
+// This module will not appear in Client Side App
+import path from 'path';
+import fs from 'fs/promises';
+
 const HomePage = (props) => {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(product => (
+        <li
+          key={product.id}>
+            {product.title}
+        </li>
+      ))}
     </ul>
   );
 };
 
+// NextJS-getStaticProps-Server-Side-Rendering
+// Code in getStaticProps is not shipped to the client
 export const getStaticProps = async () => {
-  return { props: {
+  const filePath = path.join(process.cwd(), 'data/dummy-backend.json' );
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
 
-  }};
+  return {
+    props: {
+      products: data.products
+    },
+
+    // NextJS-ISR-Incremental-Static-Regeneration
+    //  Means generate the page after every 10 seconds.
+    // It does not mean that page will be refreshed after 2 secs.
+    // It means if a new http requests comes after 2 seconds a newly
+    // updated version will be served to the client.
+    // In development server, we will always see the latest data no matter if
+    // this option is given. But in production, this section matters.
+    // This is called incremental static regeneration.
+    revalidate: 2
+  };
 };
 
 export default HomePage;
